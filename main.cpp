73becc884f457c72834b5f64aee9b4669e2b11ac
TimmPng <antcompix@gmail.com>
2021-03-23 20:13:21 +0200

    Added cactus

--- main.cpp
+++ main.cpp
@@ -6,32 +6,79 @@ const int WINDOWWIGHT = 1000;  // Window wight
 const int WINDOWHEIGHT = 600;  // Window height
 const int FPS = 60;            // FPS
 
+class Dino {
+   public:
+    sf::Texture dinoTexture;    // Creating dino texture
+    sf::Sprite dinoSprite;      // Creating dino sprite
+    int dinoWight, dinoHeight;  // Dino wight and height
+
+    // Jump
+    bool make_jump_flag = false;  // Flag make jump
+   private:
+    int jump_counter = 30;  // Jump counter
+
+   public:
+    Dino(std::string pathToTexture, int wight, int height, int x, int y) {
+        dinoTexture.loadFromFile(pathToTexture);  // Loading dino texture
+        dinoSprite.setTexture(dinoTexture);       // Seting dino texture
+        dinoSprite.setPosition(x, y);             // Seting dino position
+    }
+
+   public:
+    // Jump
+    void jump() {
+        if (jump_counter >= -30) {
+            dinoSprite.move(0, -(jump_counter / 3));
+            jump_counter -= 1;
+        } else {
+            jump_counter = 30;
+            make_jump_flag = false;
+        }
+    }
+};
+
+// Background
+class WindowBackground {
+   public:
+    sf::Texture backgoundTexture;  // Creating object texture
+    sf::Sprite backgroundSprite;   // Creating background sprite
+
+    WindowBackground(std::string pathToTexture) {
+        backgoundTexture.loadFromFile(pathToTexture);   // Loading object texture
+        backgroundSprite.setTexture(backgoundTexture);  // Seting object texture
+        backgroundSprite.setPosition(0, 0);             // Setting background position
+    }
+};
+
+class Object {
+   public:
+    sf::Texture objectTexture;  // Creating object texture
+    sf::Sprite objectSprite;    // Creating object sprite
+
+    Object(std::string pathToTexture, int wight, int height, int x, int y) {
+        objectTexture.loadFromFile(pathToTexture);  // Loading object texture
+        objectSprite.setTexture(objectTexture);     // Seting object texture
+        objectSprite.setPosition(x, y);             // Setting object position
+    }
+};
+
 int main() {
     // Creating window
     sf::RenderWindow window(sf::VideoMode(WINDOWWIGHT, WINDOWHEIGHT), "Dino", sf::Style::Close);  // Mode: video, window wight = 1000, window height = 600, name = Dino, style = Close
 
-    // Background
-    sf::Texture backgoundTexture;                               // Creating object texture
-    backgoundTexture.loadFromFile("Files/Img/BackGround.png");  // Loading object texture
-    sf::Sprite backgroundSprite;                                //  Creating background sprite
-    backgroundSprite.setTexture(backgoundTexture);              // Seting object texture
-    backgroundSprite.setPosition(0, 0);                         // Setting background position
+    // Creating dino
+    Dino dino("Files/Img/Dino_1.png", 80, 140, WINDOWWIGHT / 6, WINDOWHEIGHT - 140 - 100);
 
-    // Dino
-    sf::Texture dinoTexture;                                            // Creating dino texture
-    dinoTexture.loadFromFile("Files/Img/Dino_1.png");                   // Loading dino texture
-    int dinoWight = 80;                                                 // Dino wight
-    int dinoHeight = 140;                                               // Dino height
-    sf::Sprite dinoSprite;                                              // Creating dino sprite
-    dinoSprite.setTexture(dinoTexture);                                 // Seting dino texture
-    dinoSprite.setPosition(WINDOWWIGHT / 6, WINDOWHEIGHT - 140 - 100);  // Seting dino position
+    // Creating background window
+    WindowBackground WindowBackground("Files/Img/BackGround.png");
+
+    // Ceating cactus
+    Object cactus_1("Files/Img/Cactus_1.png", 40, 88, 40, 420);
+    Object cactus_2("Files/Img/Cactus_2.png", 49, 93, 50, 415);
+    Object cactus_3("Files/Img/Cactus_3.png", 44, 104, 45, 405);
 
     // Setting FPS
     window.setFramerateLimit(FPS);
-
-    // Jump
-    int jump_counter = 30;
-    bool mate_jump = false;
 
     // Main loop
     while (window.isOpen()) {
@@ -40,24 +87,22 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-                mate_jump = true;
+                dino.make_jump_flag = true;  // Jump
             }
         }
         // Jump
-        if (mate_jump) {
-            if (jump_counter >= -30) {
-                dinoSprite.move(0, -(jump_counter / 3));
-                jump_counter -= 1;
-            } else {
-                jump_counter = 30;
-                mate_jump = false;
-            }
+        if (dino.make_jump_flag) {
+            dino.jump();
         }
 
-        window.clear();                 // Clearing window
-        window.draw(backgroundSprite);  // Drawing background
-        window.draw(dinoSprite);        // Drawing dino
+        window.clear();                                  // Clearing window
+        window.draw(WindowBackground.backgroundSprite);  // Drawing background
+        window.draw(dino.dinoSprite);                    // Drawing dino
+        window.draw(cactus_1.objectSprite);
+        window.draw(cactus_2.objectSprite);
+        window.draw(cactus_3.objectSprite);
         window.display();
     }
+
     return 0;
 }
